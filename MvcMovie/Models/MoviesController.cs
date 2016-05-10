71f0cd23c133ac1257d884457b5b53d7e4190f28
zainkai <turkingtonkevin@gmail.com>
2016-05-10 10:17:59 -0700

    search module added

--- MvcMovie/Models/MoviesController.cs
+++ MvcMovie/Models/MoviesController.cs
@@ -14,8 +14,17 @@ namespace MvcMovie.Models
         private MovieDBContext db = new MovieDBContext();
 
         // GET: Movies
-        public ActionResult Index(string searchString)
+        public ActionResult Index(string movieGenre, string searchString)
         {
+            var GenreLst = new List<string>();
+
+            var GenreQry = from d in db.Movies
+                           orderby d.Genre
+                           select d.Genre;
+
+            GenreLst.AddRange(GenreQry.Distinct());
+            ViewBag.movieGenre = new SelectList(GenreLst);
+
             var movies = from m in db.Movies
                          select m;
 
@@ -24,7 +33,12 @@ namespace MvcMovie.Models
                 movies = movies.Where(s => s.Title.Contains(searchString));
             }
 
-            return View(movies); 
+            if (!string.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre == movieGenre);
+            }
+
+            return View(movies);
         }
 
         // GET: Movies/Details/5
